89806d5d6ff33b46997bbebf6cd66f24dd258523
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2023-09-10 10:39:50 +0000

    dump mobile-vid link for external use

--- scripts/01_vid-mid.R
+++ scripts/01_vid-mid.R
@@ -1,2 +1,12 @@
 # Code to generate the stk mobilid-localid vessel id
-#  Use: https://github.com/einarhjorleifsson/omar/blob/main/data-raw/00_SETUP_mobileid-vid-match.R
+#  See: https://github.com/einarhjorleifsson/omar/blob/main/data-raw/00_SETUP_mobileid-vid-match.R
+
+# NOTE: The datadump here is not used downstream, dump only made for "external usage"
+library(tidyverse)
+library(omar)
+con <- connect_mar()
+tbl_mar(con, "ops$einarhj.mobile_vid") |> 
+  filter(!is.na(vid))  |> 
+  collect(n = Inf) |> 
+  write_parquet("data-aux/mobile_vid.rds")
+  
