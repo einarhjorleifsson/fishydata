56ec2c74918f549a28898ce1c0434c4f73ecc02f
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2025-03-20 11:37:05 +0000

    astd slight modification

--- scripts/91_DATASET_astd-isleez.R
+++ scripts/91_DATASET_astd-isleez.R
@@ -1,18 +1,16 @@
-# nohup R < scripts/91_DATASET_astd-isleez.R --vanilla > scripts/log/91_DATASET_astd-isleez_2024-11-20.log &
+# nohup R < scripts/91_DATASET_astd-isleez.R --vanilla > scripts/log/91_DATASET_astd-isleez_2025-03-17.log &
+
+library(tictoc)
+tic()
+
+#year_now <- lubridate::today() |> lubridate::year()
+YEARS <- c(2024:2013)
 
 library(arrow)
 library(tidyverse)
 library(sf)
 
-lgs <- 
-  read_parquet("data/logbooks/stations.parquet") |> 
-  filter(year(date) >= 2013) |>  
-  group_by(vid, date) |> 
-  summarise(gid_ln_agf = min(gid_ln_agf),
-            gid = min(gid),
-            .groups = "drop")
-
-harbours <- read_sf("/u3/haf/stasi/geoserver/misc/harbours.gpkg")
+harbours <- read_sf("data/auxillary/harbours.gpkg")
 mmsi <- 
   open_dataset("data/vessels/mmsi_iceland_archieves.parquet") |> 
   filter(mmsi_cat == "vessel") |> 
@@ -26,7 +24,7 @@ astd <-
   open_dataset("data/ais/astd") |> 
   filter(eez == "ISL" | flag == "ISL")
 
-YEARS <- 2013:2024
+
 for(y in 1:length(YEARS)) {
   YEAR <- YEARS[y]
   print(YEAR)
@@ -34,10 +32,10 @@ for(y in 1:length(YEARS)) {
   trail <- 
     astd |> 
     filter(year == YEAR) |> 
-    select(.rid, mmsi, imo = imonumber, time, vessel,
+    dplyr::select(.rid, mmsi, imo = imonumber, time, vessel,
            astd_cat, dmeters = dist_nextpoint, dminutes = sec_nextpoint,
            speed,
-           lon, lat, x, y, imo_valid, flag, caff, pca, eez) |> 
+           lon, lat, imo_valid, flag, caff, pca, eez) |> 
     collect() |> 
     left_join(mmsi,
               by = join_by(mmsi, between(time, t1, t2))) |> 
@@ -64,7 +62,7 @@ for(y in 1:length(YEARS)) {
     # cruise id (aka tripid), negative values: in harbour
     arrange(mmsi, time) |> 
     group_by(mmsi) %>%
-    # NOTE: This generates trip ID within each year becuse of the loop
+    # NOTE: This generates trip ID within each year because of the loop
     mutate(.cid = ramb::rb_trip(!is.na(hid))) |> 
     ungroup() |> 
     # include first and last point in harbour as part of trip
@@ -72,15 +70,19 @@ for(y in 1:length(YEARS)) {
     mutate(.cid = case_when(.cid < 0 & lead(.cid) > 0 ~ lead(.cid),
                             .cid < 0 & lag(.cid)  > 0 ~ lag(.cid),
                             .default = .cid)) |> 
-    ungroup() |> 
-    filter(.cid > 0)
+    ungroup()
   
   trail |> 
     mutate(vid = as.integer(vid)) |> 
-    mutate(year = year(time)) |> 
-    mutate(date = as_date(time)) |> 
-    left_join(lgs) |> 
-    arrow::write_dataset("data/ais/astd_isleez", format = "parquet",
+    mutate(year = year(time)) |>
+    arrow::write_dataset("data/ais/astd_isleez", 
+                         format = "parquet",
+                         existing_data_behavior = "overwrite",
                          partitioning = c("year"))
 
 }
+
+toc()
+
+devtools::session_info()
+
