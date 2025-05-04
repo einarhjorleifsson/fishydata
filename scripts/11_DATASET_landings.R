c09649435051e6f318748c9ab6fcf76cc80b2cd7
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2025-05-04 15:13:04 +0000

    landings - use just date, hid and gid
    affects downstream logbook code

--- scripts/11_DATASET_landings.R
+++ scripts/11_DATASET_landings.R
@@ -37,6 +37,7 @@ lubridate::now()
 #       are not the same.
 YEARS <- 2025:2001
 
+library(sf)
 library(tidyverse)
 library(arrow)
 library(omar)
@@ -49,32 +50,28 @@ LN_raw <-
   omar::ln_agf(con) |> 
   filter(wt > 0,            # 2023-12-28 - lots of zero records, why?
          year(date) %in% YEARS) |> 
-  rename(# .lid = .id,  # this is NOT a landing id
-    hid_ln = hid,
-    gid_ln = gid,
-    datel = date) |> 
   collect(n = Inf) |> 
   filter(vid >= 5) |> 
   #filter(vid %in% c(2, 5:3699, 5000:9998)) |> 
-  mutate(datel = as_date(datel),
+  mutate(date = as_date(date),
          vid = as.integer(vid),
-         gid_ln = as.integer(gid_ln),
-         hid_ln = as.integer(hid_ln)) |> 
-  arrange(vid, datel, .id, gid_ln, sid) |>
-  group_by(vid, datel) |> 
+         gid = as.integer(gid),
+         hid = as.integer(hid)) |> 
+  arrange(vid, date, .id, gid, sid) |>
+  group_by(vid, date) |> 
   mutate(.lid = min(.id)) |> 
   ungroup() |> 
   # 2023-12-28: summarise the lot
-  group_by(.lid, vid, datel, hid_ln, gid_ln, sid) |> 
+  group_by(.lid, vid, date, hid, gid, sid) |> 
   summarise(wt = sum(wt),
             .groups = "drop")
 
 ### Checks
 checks <- 
   LN_raw |> 
-  group_by(vid, datel) |> 
-  summarise(n_harbours = n_distinct(hid_ln),
-            n_gears = n_distinct(gid_ln),
+  group_by(vid, date) |> 
+  summarise(n_harbours = n_distinct(hid),
+            n_gears = n_distinct(gid),
             n_lid = n_distinct(.lid),
             .groups = "drop")
 checks |> 
@@ -96,29 +93,29 @@ LN <-
   LN_raw |> 
   arrange(.lid, desc(wt)) |> # the most prevalent catch controls what
   #                            harbour and gear is kept
-  select(vid, datel, gid_ln, hid_ln, .lid) |> 
+  select(vid, date, gid, hid, .lid) |> 
   # here only keep one gid and hid record within a landings date
-  distinct(vid, datel, .keep_all = TRUE)
+  distinct(vid, date, .keep_all = TRUE)
 ### Checks, again
 checks <- 
   LN |> 
-  group_by(vid, datel) |> 
-  summarise(n_harbours = n_distinct(hid_ln),
-            n_gears = n_distinct(gid_ln),
+  group_by(vid, date) |> 
+  summarise(n_harbours = n_distinct(hid),
+            n_gears = n_distinct(gid),
             n_lid = n_distinct(.lid),
             .groups = "drop")
 checks |> 
   count(n_harbours) |> 
   mutate(p = round(n / sum(n), 3)) |> 
-  knitr::kable(caption = "Same landings date, different harbours")
+  knitr::kable(caption = "Same landings date, different harbours - expect just one")
 checks |> 
   count(n_gears) |> 
   mutate(p = round(n / sum(n), 3)) |> 
-  knitr::kable(caption = "Same landings date, different gear")
+  knitr::kable(caption = "Same landings date, different gear - expect just one")
 checks |> 
   count(n_lid) |> 
   mutate(p = round(n / sum(n), 3)) |> 
-  knitr::kable(caption = "Same landings date, different landings id (expect zero)")
+  knitr::kable(caption = "Same landings date, different landings id - expect just one")
 LN_raw |> 
   filter(!.lid %in% LN$.lid) |> 
   knitr::kable(caption = "Number of landings id lost (expect zero)")
@@ -131,18 +128,42 @@ CA <-
 
 ## Save stuff ------------------------------------------------------------------
 LN |> 
-  arrange(vid, datel) |> 
+  arrange(vid, date) |> 
   write_parquet("~/stasi/fishydata/data/landings/agf_stations.parquet")
 CA |> 
   write_parquet("~/stasi/fishydata/data/landings/agf_catch.parquet")
 LN |> 
-  arrange(vid, datel) |> 
+  arrange(vid, date) |> 
   write_parquet("/u3/haf/stasi/fishydata/data/landings/agf_stations.parquet")
 CA |> 
   write_parquet("/u3/haf/stasi/fishydata/data/landings/agf_catch.parquet")
 
+# Last landings record - used in a shiny
+tmp_gear <- 
+  nanoparquet::read_parquet("/home/haf/einarhj/stasi/fishydata/data/gear/gear_mapping.parquet") |> 
+  select(gid = gid_agf, veiðarfæri = veiðarfæri_agf) |> 
+  mutate(gear = paste(str_pad(gid, width = 2, pad = "0"), veiðarfæri)) |> 
+  select(-veiðarfæri)
 
-
+tmp_ports <- 
+  read_sf("/home/haf/einarhj/stasi/fishydata/data/ports/ports.gpkg") |> 
+  st_drop_geometry() |> 
+  select(hid, port) |> 
+  mutate(port = case_when(!is.na(hid) ~ paste(str_pad(hid, width = 3, pad = "0"), port),
+                         .default = port))
+LN |> 
+  group_by(vid) |> 
+  filter(date == max(date, na.rm = TRUE)) |> 
+  left_join(CA,
+            by = join_by(.lid)) |> 
+  left_join(tmp_gear,
+            by = join_by(gid)) |> 
+  left_join(tmp_ports,
+            by = join_by(hid)) |> 
+  select(vid, date, 
+         gear, port, sid, wt) |> 
+  write_parquet("~/stasi/fishydata/data/landings/agf_last_landing.parquet")
+  
 # LODS landings ----------------------------------------------------------------
 ## Get data --------------------------------------------------------------------
 LN_raw2 <- 
@@ -156,31 +177,28 @@ LN_raw2 <-
          gid = veidarf) |> 
   filter(wt > 0,
          year(date) %in% YEARS) |> 
-  rename(hid_ln = hid,
-         gid_ln = gid,
-         datel = date) |> 
   collect(n = Inf) |> 
   filter(vid >= 5) |> 
   #filter(vid %in% c(2, 5:3699, 5000:9998)) |> 
-  mutate(datel = as_date(datel),
+  mutate(date = as_date(date),
          vid = as.integer(vid),
-         gid_ln = as.integer(gid_ln),
-         hid_ln = as.integer(hid_ln)) |> 
-  arrange(vid, datel, .id, gid_ln, sid) |>
-  group_by(vid, datel) |> 
+         gid = as.integer(gid),
+         hid = as.integer(hid)) |> 
+  arrange(vid, date, .id, gid, sid) |>
+  group_by(vid, date) |> 
   mutate(.lid = min(.id)) |> 
   ungroup() |> 
   # 2023-12-28: summarise the lot
-  group_by(.lid, vid, datel, hid_ln, gid_ln, sid) |> 
+  group_by(.lid, vid, date, hid, gid, sid) |> 
   summarise(wt = sum(wt),
             .groups = "drop")
 
 ### Checks
 checks <- 
   LN_raw2 |> 
-  group_by(vid, datel) |> 
-  summarise(n_harbours = n_distinct(hid_ln),
-            n_gears = n_distinct(gid_ln),
+  group_by(vid, date) |> 
+  summarise(n_harbours = n_distinct(hid),
+            n_gears = n_distinct(gid),
             n_lid = n_distinct(.lid),
             .groups = "drop")
 checks |> 
@@ -202,15 +220,15 @@ LN2 <-
   LN_raw2 |> 
   arrange(.lid, desc(wt)) |> # the most prevalent catch controls what
   #                            harbour and gear is kept
-  select(vid, datel, gid_ln, hid_ln, .lid) |> 
+  select(vid, date, gid, hid, .lid) |> 
   # here only keep one gid and hid record within a landings date
-  distinct(vid, datel, .keep_all = TRUE)
+  distinct(vid, date, .keep_all = TRUE)
 ### Checks, again
 checks <- 
   LN2 |> 
-  group_by(vid, datel) |> 
-  summarise(n_harbours = n_distinct(hid_ln),
-            n_gears = n_distinct(gid_ln),
+  group_by(vid, date) |> 
+  summarise(n_harbours = n_distinct(hid),
+            n_gears = n_distinct(gid),
             n_lid = n_distinct(.lid),
             .groups = "drop")
 checks |> 
@@ -238,12 +256,12 @@ CA2 <-
   mutate(wt = ifelse(sid %in% c(30, 31, 34), wt * 1e3, wt))
 ## Save stuff ------------------------------------------------------------------
 LN2 |> 
-  arrange(vid, datel) |> 
+  arrange(vid, date) |> 
   write_parquet("~/stasi/fishydata/data/landings/lods_stations.parquet")
 CA2 |> 
   write_parquet("~/stasi/fishydata/data/landings/lods_catch.parquet")
 LN2 |> 
-  arrange(vid, datel) |> 
+  arrange(vid, date) |> 
   write_parquet("/u3/haf/stasi/fishydata/data/landings/lods_stations.parquet")
 CA2 |> 
   write_parquet("/u3/haf/stasi/fishydata/data/landings/lods_catch.parquet")
@@ -255,33 +273,33 @@ if(TRUE) {
   nrow(LN2)
   check <- 
     LN |> 
-    select(vid, datel, .lid_agf = .lid) |> 
+    select(vid, date, .lid_agf = .lid) |> 
     full_join(LN2 |> 
-                select(vid, datel, .lid_lods = .lid)) |> 
+                select(vid, date, .lid_lods = .lid)) |> 
     mutate(what = case_when(!is.na(.lid_agf) & !is.na(.lid_lods) ~ "both",
                             !is.na(.lid_agf) &  is.na(.lid_lods) ~ "agf",
                             is.na(.lid_agf) & !is.na(.lid_lods) ~ "lods",
-                            .default = NA)) 
+                            .default = NA))
   check |> count(what)
   check |> 
-    mutate(year = year(datel)) |> 
+    mutate(year = year(date)) |> 
     count(year, what) |> 
     spread(what, n) |> 
-    knitr::kable(caption = "Difference in vid-datel records in AGF vs LODS")
+    knitr::kable(caption = "Difference in vid-date records in AGF vs LODS")
   # So mostly in 2022 where 13% of records in LODS but not in AGF, needs checking
   #  This is mostly associated with period of 2022-05-30 to 2022-06-16
   
   # Catch records are somewhat differernt:
   CA_2009_2023 <-
     LN |> 
-    filter(year(datel) %in% 2009:2024) |> 
+    filter(year(date) %in% 2009:2024) |> 
     select(.lid) |> 
     left_join(CA) |>
     group_by(sid) |> 
     summarise(wt_agf = sum(wt) / 1e6)
   CA2_2009_2023 <-
     LN2 |> 
-    filter(year(datel) %in% 2009:2024) |> 
+    filter(year(date) %in% 2009:2024) |> 
     select(.lid) |> 
     left_join(CA2) |>
     group_by(sid) |> 
